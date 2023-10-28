617f88dae64af4c4cc19f7a26fff146e15fd860a
Biraj Ghosh <birajghosh6@gmail.com>
2023-10-28 09:05:32 -0700

    Initial commit

---
+++ database/factories/PlayerFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Player;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PlayerFactory extends Factory
+{
+    protected $model = Player::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'addressLine1' => $this->faker->sentence(1),
+            'addressLine2' => $this->faker->sentence(1),
+            'city' => $this->faker->sentence(1),
+            'province' => $this->faker->sentence(1),
+            'country' => $this->faker->sentence(1),
+            'postal' => $this->faker->randomNumber(6),
+        ];
+    }
+}
---
+++ database/migrations/2023_10_28_154326_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('addressLine1');
+            $table->string('addressLine2');
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('postal');
+            $table->integer('points')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
